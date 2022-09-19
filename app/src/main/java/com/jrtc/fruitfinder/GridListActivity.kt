7927bf4e97f26283318af73bb864cf69026f8805
Carson Saldanha <carson.saldanha@gmail.com>
2022-09-19 15:45:41 -0500

    Grid layout fix

--- app/src/main/java/com/jrtc/fruitfinder/GridListActivity.kt
+++ app/src/main/java/com/jrtc/fruitfinder/GridListActivity.kt
@@ -3,14 +3,15 @@ package com.jrtc.fruitfinder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.jrtc.fruitfinder.adapter.FruitCardAdapter
+import com.jrtc.fruitfinder.databinding.ActivityMainBinding
 
 class GridListActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityGridListBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityGridListBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.gridRecyclerView.adapter = FruitCardAdapter(applicationContext)
--- app/src/main/java/com/jrtc/fruitfinder/MainActivity.kt
+++ app/src/main/java/com/jrtc/fruitfinder/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.jrtc.fruitfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jrtc.fruitfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Setup view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Launch the GridListActivity
+        listIntent = Intent(this, GridListActivity::class.java)
+        startActivity(listIntent)
     }
+
 }

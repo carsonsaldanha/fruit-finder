83989354d37e061c9b43c46747fc068b90c037e0
Carson Saldanha <carson.saldanha@gmail.com>
2022-09-19 15:55:07 -0500

    Main activity layout fix

--- app/src/main/java/com/jrtc/fruitfinder/MainActivity.kt
+++ app/src/main/java/com/jrtc/fruitfinder/MainActivity.kt
@@ -3,12 +3,12 @@ package com.jrtc.fruitfinder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jrtc.fruitfinder.adapter.FruitCardAdapter
 import com.jrtc.fruitfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var listIntent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +16,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Launch the GridListActivity
-        listIntent = Intent(this, GridListActivity::class.java)
-        startActivity(listIntent)
+        binding.gridRecyclerView.adapter = FruitCardAdapter(applicationContext)
+
+        // Specify fixed size to improve performance
+        binding.gridRecyclerView.setHasFixedSize(true)
     }
 
 }
